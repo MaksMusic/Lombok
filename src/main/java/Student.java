1db70bb5fb10e371e524a96639c86c4da8e0d475
MaksMusic <Maksmusic05@yandex.ru>
2023-08-19 12:43:20 +0300

    addd class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,6 @@
 import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
 
 
 @Data
@@ -13,7 +15,7 @@ public class Student {
 
 @AllArgsConstructor
 @RequiredArgsConstructor
-class Employee{
+class Employee {
     private String name;
     @NonNull
     private Long id;
@@ -21,16 +23,45 @@ class Employee{
 }
 
 
-class Main{
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+class SmartPhone {
+    Long id;
+    @NonNull
+    String model;
+    @NonNull
+    double price;
+
+}
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
+class Elf{
+    String name;
+    double str;
+    double hp;
+
+}
+
+
+class Main {
     public static void main(String[] args) {
-        Student student = new Student("Tom",22);
-        Student student2 = new Student("Tom",22);
+        Elf elf = Elf.builder().name("Falcone").str(200).build();
+        System.out.println(elf);
+        SmartPhone smartPhone = new SmartPhone("samsung",1000);
+
+
+        Student student = new Student("Tom", 22);
+        Student student2 = new Student("Tom", 22);
 
         System.out.println(student);
         System.out.println(student.equals(student2));
         System.out.println(student.getAge());
 
-        Employee employee = new Employee("Tom",1L);
+        Employee employee = new Employee("Tom", 1L);
 
     }
 }
